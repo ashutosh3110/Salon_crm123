5d770e37ad24d56d8b90730e9efae7638abead92
ashu3110 <ashutoshbankey21306@gmail.com>
2026-04-04 13:23:37 +0530

    fix error

--- salon_frontend/src/services/pushNotification.js
+++ salon_frontend/src/services/pushNotification.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import api from './api';
 
@@ -11,7 +11,7 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
 export const requestForToken = async () => {
